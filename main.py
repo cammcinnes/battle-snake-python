8267c7bb6f27e6653644bbacf44ec65535faa418
Cam McInnes <camtmcinnes@gmail.com>
2024-07-11 12:49:06 -0700

    customize my battlesnake

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def info() -> typing.Dict:
 
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        "author": "cammcinnes",  # TODO: Your Battlesnake Username
+        "color": "#000000",  # TODO: Choose color
+        "head": "pixel",  # TODO: Choose head
+        "tail": "pixel",  # TODO: Choose tail
     }
 
 
